c0cb097024c065344fd859aacaa0bae4f52d312b
sonu3009 <sonurajpoot.rajpoot6@gmail.com>
2019-11-02 14:03:19 +0530

    Initial testing

---
+++ build/precache-manifest.7f9c5e87d31cd6a548466a9f0c470424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab13e9a54bd69a04c62aca8b02193468",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9180febffce2145b17c",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "49a536d2132ff433f0ab",
+    "url": "/static/js/2.5a570ff8.chunk.js"
+  },
+  {
+    "revision": "b9180febffce2145b17c",
+    "url": "/static/js/main.c23d3e25.chunk.js"
+  },
+  {
+    "revision": "e7f4b15778e6afbda45a",
+    "url": "/static/js/runtime~main.f540eabf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
